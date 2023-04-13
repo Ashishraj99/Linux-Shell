47c74141664c5b79742278705a94538e9032bf37
Ashishraj99 <81153070+Ashishraj99@users.noreply.github.com>
2023-04-13 17:08:00 +0530

    Update main.tf

--- terraform 2/main.tf
+++ terraform 2/main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  access_key = "AKIAWJN3TAEU5MN2Z2M2"
-  secret_key = "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"  
+  access_key = ""
+  secret_key = ""  
   region = var.region
 }
